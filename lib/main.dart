fda1e1bd579904d64e3c8defc9b51a47094bffd5
Your Name <your-email@example.com>
2026-01-21 17:45:26 +0530

    done

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(CounterApp());
+
+class CounterApp extends StatefulWidget {
+  @override
+  _CounterAppState createState() => _CounterAppState();
+}
+
+class _CounterAppState extends State<CounterApp> {
+  int count = 0;
+
+  void increment() {
+    setState(() {
+      count++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Flutter Assignment')),
+        body: Center(
+          child: Text(
+            'Count: $count',
+            style: TextStyle(fontSize: 30),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: increment,
+          child: Icon(Icons.add),
+        ),
+      ),
+    );
+  }
+}
